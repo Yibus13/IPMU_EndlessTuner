31720b90cb10965b4e7787a8e1ba64119d042e05
LucasAH9 <lucasandosilla@gmail.com>
2022-11-22 20:30:49 +0100

    Arreglado salto infinito y limites laterales

--- EndlessRunner IPMU/Assets/Scripts/PlayerMovement.cs
+++ EndlessRunner IPMU/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine.SceneManagement;
 public class PlayerMovement : MonoBehaviour
 {
     public LayerMask groundMask;
-    public float jumpForce = 400f;
+
+
     bool alive = true;
     public float speed = 5f;
     public float acceleration = 0.05f;
@@ -13,13 +14,32 @@ public class PlayerMovement : MonoBehaviour
     float horizontalInput;
     public float horizontalMult = 2f;
 
+    [SerializeField] 
+    public float jumpForce = 600f;
+    
+
+
+
     void FixedUpdate()
     {
-        if(!alive){
+        if (!alive) {
             return;
         }
         Vector3 forwardMov = transform.forward * speed * Time.fixedDeltaTime;
+        
+        
+   
+        if(rb.position.x <= -5 && horizontalInput < 0)
+        {
+            horizontalInput = 0;
+        }
+        else if(rb.position.x >= 5 && horizontalInput > 0)
+        {
+            horizontalInput = 0;
+        }
+
         Vector3 horizontalMov = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMult;
+
         rb.MovePosition(rb.position + forwardMov + horizontalMov);
         //speed += acceleration;
     }
@@ -30,14 +50,22 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
 
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
             Jump();
         }
 
-        if(transform.position.y < -5){
+        if (transform.position.y < -5) {
             Death();
         }
     }
+
+    public bool IsGrounded() {
+        float height = GetComponent<Collider>().bounds.size.y;
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, (height / 2) + 0.1f, groundMask);
+       
+        return isGrounded;
+    }
+
     public void Death(){
         alive = false;
         Invoke("RestartGame", 2);
@@ -47,9 +75,7 @@ public class PlayerMovement : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     void Jump(){
-        float height = GetComponent<Collider>().bounds.size.y;
-        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, (height / 2) + 0.1f, groundMask);
-
+       
         rb.AddForce(Vector3.up * jumpForce);
     }
 }
